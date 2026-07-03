0d9302205e9200932524a6a388d3d4e25707c042
Brencovered <team@swiftscope.com.au>
2026-07-03 10:50:33 +1000

    feat: add SEO keywords page

---
+++ app/electrician/seo/page.tsx
@@ -0,0 +1,7 @@
+import SeoKeywordsPanel from "@/components/SeoKeywordsPanel";
+
+export const metadata = { title: "SEO Keywords - Swiftscope" };
+
+export default function SeoPage() {
+  return <SeoKeywordsPanel />;
+}
