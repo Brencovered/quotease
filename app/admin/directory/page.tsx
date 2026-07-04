e99394afcf24607e452acad52a3d2242361d8671
Brencovered <team@swiftscope.com.au>
2026-07-04 12:11:24 +1000

    feat: admin directory listings page

---
+++ app/admin/directory/page.tsx
@@ -0,0 +1,7 @@
+import AdminDirectoryPanel from "@/components/AdminDirectoryPanel";
+
+export const dynamic = "force-dynamic";
+
+export default function AdminDirectoryPage() {
+  return <AdminDirectoryPanel />;
+}
