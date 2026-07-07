b1deac17a56bce4f0d3fa0f7f49503292cc4d17e
Brencovered <team@swiftscope.com.au>
2026-07-07 17:27:05 +1000

    Delete old /electrician/seo/page.tsx - moved to /admin/seo

--- app/electrician/seo/page.tsx
+++
@@ -1,7 +0,0 @@
-import SeoKeywordsPanel from "@/components/SeoKeywordsPanel";
-
-export const metadata = { title: "SEO Keywords - Swiftscope" };
-
-export default function SeoPage() {
-  return <SeoKeywordsPanel />;
-}
